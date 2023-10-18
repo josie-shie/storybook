1aed815a62eca23749f8831b46d5112b66f1d510
Zafer <zafer@befuture8.com>
2023-10-18 11:17:30 +0800

    feat: site-03 tag component

--- apps/site-03/components/tag/tag.tsx
+++ apps/site-03/components/tag/tag.tsx
@@ -1,11 +1,23 @@
+import type { ReactElement } from 'react';
 import style from './tag.module.scss';
 
 interface PropsType {
+    icon?: ReactElement;
     text?: string;
+    gradient?: 'blue';
+    color?: string;
+    background?: string;
 }
 
-function Tag({ text }: PropsType) {
-    return <span className={style.tag}>{text}</span>;
+function Tag({ icon, text, gradient, color, background }: PropsType) {
+    const gradientClass = gradient === 'blue' ? style.gradientBlue : '';
+
+    return (
+        <div className={`${style.tag} ${gradientClass}`} style={{ background, color }}>
+            {icon ? <span className={style.tagIcon}>{icon}</span> : null}
+            <span>{text}</span>
+        </div>
+    );
 }
 
 export default Tag;
