9ef2347a8d34fa7fafe966507032c56ac6cbabf4
Ben <ben@befuture8.com>
2023-10-19 14:20:02 +0800

    feat: guess page add rank list

--- apps/site-03/app/recommend/layout.tsx
+++ apps/site-03/app/recommend/layout.tsx
@@ -2,13 +2,25 @@
 import type { ReactNode } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { useEffect } from 'react';
-import Header from '../components/headerSwitch/header';
+// import Header from '../components/headerSwitch/header';
+import Header from '../components/header/headerTabs';
+import Footer from '../components/footer/footer';
 import { Tabs } from '../components/tabs/tabs';
+import Logo from '../components/header/img/logo.svg';
 import style from './recommend.module.scss';
 
 function RecommendLayout({ children }: { children: ReactNode }) {
     const router = useRouter();
     const paramName = usePathname();
+
+    const headerProps = {
+        logo: <Logo />,
+        tabList: [
+            { label: '足球', value: 'football' },
+            { label: '籃球', value: 'basketball' }
+        ],
+        total: 999999
+    };
 
     useEffect(() => {
         if (paramName === '/recommend') {
@@ -18,7 +30,11 @@ function RecommendLayout({ children }: { children: ReactNode }) {
 
     return (
         <>
-            <Header />
+            <Header
+                logo={headerProps.logo}
+                tabList={headerProps.tabList}
+                total={headerProps.total}
+            />
             <div className={style.layout}>
                 <Tabs
                     labels={['競猜', '專家預測', '大數據分析']}
@@ -26,6 +42,7 @@ function RecommendLayout({ children }: { children: ReactNode }) {
                 />
                 {children}
             </div>
+            <Footer />
         </>
     );
 }
