b1971f96c3be19868a9dea8ac204f870cfa42014
susanna <susanna@future8.com>
2023-10-19 13:46:07 +0800

    chore: move site-03 inner components to outer components

--- apps/site-03/app/recommend/layout.tsx
+++ apps/site-03/app/recommend/layout.tsx
@@ -2,13 +2,20 @@
 import type { ReactNode } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
 import { useEffect } from 'react';
-import Header from '../components/headerSwitch/header';
-import { Tabs } from '../components/tabs/tabs';
 import style from './recommend.module.scss';
+import Logo from './img/logo.svg';
+import Header from '@/components/header/headerTabs';
+import { Tabs } from '@/components/tabs/tabs';
 
 function RecommendLayout({ children }: { children: ReactNode }) {
     const router = useRouter();
     const paramName = usePathname();
+
+    const headerProps = {
+        logo: <Logo />,
+        total: 999999,
+        tabList: ['足球', '籃球']
+    };
 
     useEffect(() => {
         if (paramName === '/recommend') {
@@ -18,7 +25,11 @@ function RecommendLayout({ children }: { children: ReactNode }) {
 
     return (
         <>
-            <Header />
+            <Header
+                logo={headerProps.logo}
+                tabList={headerProps.tabList}
+                total={headerProps.total}
+            />
             <div className={style.layout}>
                 <Tabs
                     labels={['競猜', '專家預測', '大數據分析']}
