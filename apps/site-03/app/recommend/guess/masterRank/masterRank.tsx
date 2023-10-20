a3ad4606606536e449e94168e1a7d27e3d71a653
Zafer <zafer@befuture8.com>
2023-10-20 13:48:52 +0800

    feat: userHotStreak layout

--- apps/site-03/app/recommend/guess/masterRank/masterRank.tsx
+++ apps/site-03/app/recommend/guess/masterRank/masterRank.tsx
@@ -1,11 +1,20 @@
 'use client';
 import { IconSearch } from '@tabler/icons-react';
+import Image from 'next/image';
 import HotStreakListItem from '../components/hotStreak/hotStreakListItem';
 import UserSwitch from '../components/userSwitch/userSwitch';
 import Rule from '../components/rule/rule';
+import Streak from '../components/hotStreak/img/streak.png';
 import style from './masterRank.module.scss';
+import Avatar from '@/components/avatar/avatar';
 
 function RankList() {
+    const user = {
+        ranking: 87,
+        name: '郭台銘',
+        currentStreak: 9,
+        highestStreak: 14
+    };
     const list = [
         {
             ranking: 1,
@@ -61,6 +70,30 @@ function RankList() {
                         <span>搜尋</span>
                     </div>
                     <Rule />
+                </div>
+            </div>
+            <div className={style.userHotStreak}>
+                <div className={style.ranking}>
+                    你排名<span>24</span>
+                </div>
+                <div className={style.container}>
+                    <div className={style.avatarContainer}>
+                        <Avatar />
+                    </div>
+                    <div className={style.content}>
+                        <div className={style.name}>{user.name}</div>
+                        <div className={style.streak}>
+                            <div className={style.current}>
+                                <Image alt="streakIcon" className={style.streakIcon} src={Streak} />
+                                <span className={style.label}>当前:</span> {user.currentStreak}連紅
+                            </div>
+                            <div className={style.highest}>
+                                <Image alt="streakIcon" className={style.streakIcon} src={Streak} />
+                                <span className={style.label}>历史最高:</span> {user.highestStreak}
+                                連紅
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             {list.map(item => (
