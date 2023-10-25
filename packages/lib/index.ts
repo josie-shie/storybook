88fb056286aa38dc4626daf1a67163067fbc364c
Roman <roman@chenzhirus-MacBook-Pro.local>
2023-10-25 18:20:57 +0800

    feat(lib): format function

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,5 +1,6 @@
 export { fetcher } from './src/fetcher';
 export { uploadImage } from './src/uploadImage';
 export { initStore } from './src/store';
-export { truncateFloatingPoint, handicapToString } from './src/formatNumber';
+export { truncateFloatingPoint } from './src/formatNumber';
+export { handicapToString, convertHandicap } from './src/formatHandicap';
 export type { StoreWithSelectors } from './src/store';
