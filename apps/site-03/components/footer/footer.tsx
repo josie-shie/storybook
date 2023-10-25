e75aa38ebd8349d11b90d57e6ecb5d41fd82770e
susanna <susanna@future8.com>
2023-10-25 11:02:48 +0800

    feat: update storybook footer style and event handler

--- apps/site-03/components/footer/footer.tsx
+++ apps/site-03/components/footer/footer.tsx
@@ -1,42 +1,52 @@
 'use client';
 import { Footer } from 'ui';
-import HomeIcon from './img/homeIcon.svg';
-import GameIcon from './img/game.svg';
-import RecommendIcon from './img/recommend.svg';
-import DataIcon from './img/data.svg';
-import NewsIcon from './img/news.svg';
+import { usePathname } from 'next/navigation';
+import HomeIcon from './img/homeIcon';
+import GameIcon from './img/game';
+import RecommendIcon from './img/recommend';
+import DataIcon from './img/data';
+import NewsIcon from './img/news';
 import './footer.module.scss';
 
 const CategoryList = [
     {
         label: '首頁',
         value: '/',
-        icon: <HomeIcon />
+        icon: <HomeIcon fill="#AAAAAA" />
     },
     {
         label: '賽事',
         value: '/contest/football',
-        icon: <GameIcon />
+        icon: <GameIcon fill="#AAAAAA" />
     },
     {
         label: '推薦',
         value: '/recommend/guess',
-        icon: <RecommendIcon />
+        icon: <RecommendIcon fill="#AAAAAA" />
     },
     {
         label: '數據',
         value: '/data',
-        icon: <DataIcon />
+        icon: <DataIcon fill="#AAAAAA" />
     },
     {
         label: '消息',
         value: '/message',
-        icon: <NewsIcon />
+        icon: <NewsIcon fill="#AAAAAA" />
     }
 ];
 
 function FooterComponent() {
-    return <Footer activeColor="#276CE1" bgColor="#FFF" menuList={CategoryList} />;
+    const pathname = usePathname();
+
+    return (
+        <Footer
+            activeColor="#276CE1"
+            activeRouter={pathname}
+            bgColor="#FFF"
+            menuList={CategoryList}
+        />
+    );
 }
 
 export default FooterComponent;
