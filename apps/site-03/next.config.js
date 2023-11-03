b2cf4ecd9b393ca13cff25ecb52351a46ba0651f
Johnny <johnny@befuture8.com>
2023-11-03 13:46:42 +0800

    fix: next image domain

--- apps/site-03/next.config.js
+++ apps/site-03/next.config.js
@@ -5,6 +5,15 @@
 const path = require('node:path');
 
 module.exports = {
+    images: {
+        domains: [
+            'zq.titan007.com',
+            'stickershop.line-scdn.net',
+            'twtest8.s3.ap-northeast-1.amazonaws.com',
+            'twtest8.s3.amazonaws.com',
+            'fastly.picsum.photos'
+        ]
+    },
     reactStrictMode: true,
     transpilePackages: ['ui', 'lib'],
     sassOptions: {
