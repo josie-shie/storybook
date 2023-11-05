c60de733cf5dcaa6282d7e5f4f855c1cb4f5218f
lin <weiyandesign@gmail.com>
2023-11-05 22:35:13 +0800

    feat(contest): game status match server client dom

--- packages/lib/src/gameStatus.ts
+++ packages/lib/src/gameStatus.ts
@@ -1,3 +1,4 @@
+'use client';
 import dayjs from 'dayjs';
 
 export const handleStartTime = (startTime: string, endTime?: string) => {
--- packages/ui/stories/gameStatus/gameStatus.tsx
+++ packages/ui/stories/gameStatus/gameStatus.tsx
@@ -3,8 +3,26 @@ import { useState, useEffect, useRef } from 'react';
 import { handleGameTime } from 'lib';
 import style from './gameStatus.module.scss';
 
+function useGameTime({ startTime, status }: { status: number; startTime: string }) {
+    const [realTimeStatus, setRealTimeStatus] = useState<{
+        time?: number;
+        state: string;
+        text?: string;
+    }>({
+        time: 0,
+        state: '',
+        text: ''
+    });
+
+    useEffect(() => {
+        setRealTimeStatus(handleGameTime(startTime, status));
+    }, [startTime, status]);
+
+    return realTimeStatus;
+}
+
 function GameStatus({ startTime, status }: { status: number; startTime: string }) {
-    const realTimeStatus = handleGameTime(startTime, status);
+    const realTimeStatus = useGameTime({ startTime, status });
     const [realMinute, setRealMinute] = useState(realTimeStatus.time || 0);
     const timerRef = useRef<NodeJS.Timeout | null>(null);
 
