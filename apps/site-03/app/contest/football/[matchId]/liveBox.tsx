9645f16dd39f70a4c002c2a4bd68db81555b14c6
Johnny <johnny@befuture8.com>
2023-11-06 17:11:48 +0800

    feat: live box update

--- apps/site-03/app/contest/football/[matchId]/liveBox.tsx
+++ apps/site-03/app/contest/football/[matchId]/liveBox.tsx
@@ -53,23 +53,16 @@ function GameDetail() {
                     startTime={matchDetail.startTime}
                     status={matchDetail.state}
                 />
-                {liveState === -1 ||
-                    (liveState > 0 && (
-                        <>
-                            <div className={style.homeScore}>{homeLiveScore}</div>
-                            <div className={style.awayScore}>{awayLiveScore}</div>
-                        </>
-                    ))}
+
+                <div className={style.homeScore}>{homeLiveScore}</div>
+                <div className={style.awayScore}>{awayLiveScore}</div>
             </div>
 
-            {liveState === -1 ||
-                (liveState > 1 && (
-                    <div className={style.textHolder}>
-                        <p className={style.text}>
-                            半場 {matchDetail.homeHalfScore}-{matchDetail.awayHalfScore}
-                        </p>
-                    </div>
-                ))}
+            <div className={style.textHolder}>
+                <p className={style.text}>
+                    半場 {matchDetail.homeHalfScore}-{matchDetail.awayHalfScore}
+                </p>
+            </div>
         </div>
     );
 }
