5ec1a0baa03feb400d956de16bd4be56de9503f8
Zafer <zafer@befuture8.com>
2023-11-09 11:27:26 +0800

    feat: modify games filter layout

--- apps/site-03/app/recommend/predict/components/filterItem/filterItem.tsx
+++ apps/site-03/app/recommend/predict/components/filterItem/filterItem.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 import style from './filterItem.module.scss';
+import iconList from './img/list.png';
 
 interface PropsType {
     id: number;
@@ -20,18 +23,26 @@ function FilterItem({ item }: { item: PropsType }) {
 
     return (
         <div className={style.filterItem}>
-            <div className={style.top}>
-                <span className={style.name}>{item.gameName}</span>
-                <span className={style.time}>{item.gameTime}</span>
+            <div className={style.left}>
+                <div className={style.top}>
+                    <span className={style.name}>{item.gameName}</span>
+                    <span className={style.time}>{item.gameTime}</span>
+                </div>
+                <div className={style.game}>
+                    <span className={style.home}>{item.homeTeam}</span>
+                    <span className={style.compete}>VS</span>
+                    <span className={style.away}>{item.awayTeam}</span>
+                </div>
             </div>
-            <div className={style.game}>
-                <span className={style.home}>{item.homeTeam}</span>
-                <span className={style.compete}>VS</span>
-                <span className={style.away}>{item.awayTeam}</span>
-            </div>
-            <button className={style.article} onClick={goDetail} type="button">
-                {item.articleNumber}篇专家文章
-            </button>
+            <motion.button
+                className={style.article}
+                onClick={goDetail}
+                type="button"
+                whileTap={{ scale: 0.9 }}
+            >
+                <Image alt="icon" height={16} src={iconList} width={16} />
+                {item.articleNumber}篇文章
+            </motion.button>
         </div>
     );
 }
