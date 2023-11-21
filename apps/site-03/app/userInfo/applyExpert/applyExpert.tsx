67a826cde061664f09bcebf5ce111c7d66cf29b0
Ben <ben@befuture8.com>
2023-11-21 17:49:49 +0800

    feat: apply expert store

--- apps/site-03/app/userInfo/applyExpert/applyExpert.tsx
+++ apps/site-03/app/userInfo/applyExpert/applyExpert.tsx
@@ -5,10 +5,27 @@ import FullBg from '../img/fullbg.png';
 import backLeftArrowImg from '../img/backLeftArrow.png';
 import Cricle from '../img/cricle.png';
 import Banner from '../img/banner.png';
+import { createExpertApplyStore, useExpertApplyStore } from './applyExpertStore';
 import style from './applyExpert.module.scss';
 
 function ApplyExpert() {
     const router = useRouter();
+
+    createExpertApplyStore({
+        applicationProgress: {
+            fanCount: 10,
+            weeklyWinRate: 80,
+            monthlyWinRate: 80,
+            seasonWinRate: 80
+        }
+    });
+
+    const apply = useExpertApplyStore.use.applicationProgress();
+
+    // 計算勝率是否有達成判斷
+    const isEligibleToSubmit =
+        apply.fanCount >= 100 &&
+        (apply.weeklyWinRate >= 80 || apply.monthlyWinRate >= 80 || apply.seasonWinRate >= 80);
 
     return (
         <div className={style.wrapper} style={{ backgroundImage: `url(${FullBg.src})` }}>
@@ -42,23 +59,27 @@ function ApplyExpert() {
 
                     <div className={style.column}>
                         <div className={style.title}>粉丝数达到100人</div>
-                        <div className={style.content}>當前進度 100/100</div>
+                        <div className={style.content}>當前進度 100/{apply.fanCount}</div>
                     </div>
                     <div className={style.column}>
                         <div className={style.title}>周榜胜率达80%(含)以上</div>
-                        <div className={style.content}>当前胜率 0</div>
+                        <div className={style.content}>当前胜率 {apply.weeklyWinRate}%</div>
                     </div>
                     <div className={style.column}>
                         <div className={style.title}>月榜胜率达80%(含)以上</div>
-                        <div className={style.content}>当前胜率 0</div>
+                        <div className={style.content}>当前胜率 {apply.monthlyWinRate}%</div>
                     </div>
                     <div className={style.column}>
                         <div className={style.title}>季榜胜率达80%(含)以上</div>
-                        <div className={style.content}>当前胜率 0</div>
+                        <div className={style.content}>当前胜率 {apply.seasonWinRate}%</div>
                     </div>
                 </div>
 
-                <button className={style.submit} type="button">
+                <button
+                    className={`${style.submit} ${isEligibleToSubmit ? style.active : ''}`}
+                    disabled={!isEligibleToSubmit}
+                    type="button"
+                >
                     提交申请
                 </button>
             </div>
