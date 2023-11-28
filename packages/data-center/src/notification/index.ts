7f0fb581ee1609d289d9f38afeff1eaa0784c0eb
Roman <roman@chenzhirus-MacBook-Pro.local>
2023-11-28 16:00:43 +0800

    feat(dataCenter): notification mail api

---
+++ packages/data-center/src/notification/index.ts
@@ -0,0 +1,92 @@
+import { fetcher } from 'lib';
+import { z } from 'zod';
+import { handleApiError } from '../common';
+import type { ReturnData } from '../common';
+import { GET_MAIL_MEMBER_LIST_QUERY, GET_MAIL_MEMBER_QUERY } from './graphqlQueries';
+
+const GetMailMemberSchema = z.object({
+    mailMemberId: z.number(),
+    title: z.string(),
+    content: z.string(),
+    isRead: z.boolean(),
+    createdAt: z.number()
+});
+
+const GetMailMemberListSchema = z.array(GetMailMemberSchema);
+export type GetMailMemberResponse = z.infer<typeof GetMailMemberSchema>;
+
+const GetMailMemberListResultSchema = z.object({
+    getMailMemberList: z.object({
+        mailMemberList: z.array(GetMailMemberSchema)
+    })
+});
+
+type GetMailMemberListResult = z.infer<typeof GetMailMemberListResultSchema>;
+
+export type GetMailMemberListResponse = z.infer<typeof GetMailMemberListSchema>;
+
+export interface GetMailMemberRequest {
+    mailMemberId: number;
+}
+
+const GetMailMemberResultSchema = z.object({
+    getMailMember: GetMailMemberSchema
+});
+
+type GetMailMemberResult = z.infer<typeof GetMailMemberResultSchema>;
+
+/**
+ * 取得站內信列表
+ * - returns : {@link GetMailMemberListResponse}
+ * - {@link GetMailMemberResponse}
+ */
+export const getMailMemberList = async (): Promise<ReturnData<GetMailMemberListResponse>> => {
+    try {
+        const { data }: { data: GetMailMemberListResult } = await fetcher(
+            {
+                data: {
+                    query: GET_MAIL_MEMBER_LIST_QUERY
+                }
+            },
+            { cache: 'no-store' }
+        );
+
+        return {
+            success: true,
+            data: data.getMailMemberList.mailMemberList
+        };
+    } catch (error) {
+        return handleApiError(error);
+    }
+};
+
+/**
+ * 取得站內信
+ * - returns : {@link GetMailMemberResponse}
+ */
+export const getMailMember = async ({
+    mailMemberId
+}: GetMailMemberRequest): Promise<ReturnData<GetMailMemberResponse>> => {
+    try {
+        const { data }: { data: GetMailMemberResult } = await fetcher(
+            {
+                data: {
+                    query: GET_MAIL_MEMBER_QUERY,
+                    variables: {
+                        input: {
+                            mailMemberId
+                        }
+                    }
+                }
+            },
+            { cache: 'no-store' }
+        );
+
+        return {
+            success: true,
+            data: data.getMailMember
+        };
+    } catch (error) {
+        return handleApiError(error);
+    }
+};
