3f9b6edea142e80411df808bfbac180a9507d9ed
susanna <susanna@future8.com>
2023-11-30 16:01:55 +0800

    style: fix global notification bug

--- apps/site-03/components/notification/notification.tsx
+++ apps/site-03/components/notification/notification.tsx
@@ -1,6 +1,5 @@
 import { Alert } from '@mui/material';
 import SlideTransition from './slideTransition';
-import style from './notification.module.scss';
 
 interface PropsType {
     message: string;
@@ -11,11 +10,9 @@ interface PropsType {
 
 function Notification({ message, type, isVisible, handleClose }: PropsType) {
     return (
-        <div className={style.slideTransition__position}>
-            <SlideTransition onClose={handleClose} show={isVisible}>
-                <Alert severity={type}>{message}</Alert>
-            </SlideTransition>
-        </div>
+        <SlideTransition onClose={handleClose} show={isVisible}>
+            <Alert severity={type}>{message}</Alert>
+        </SlideTransition>
     );
 }
 
