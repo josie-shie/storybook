c30e0c37d41438ad56b2c3381235f330ec6a85ed
susanna <susanna@future8.com>
2023-11-30 18:51:16 +0800

    feat: add global userInfo

--- apps/site-03/app/contestStoreHandler.tsx
+++ apps/site-03/app/contestStoreHandler.tsx
@@ -1,9 +1,18 @@
 'use client';
 import { useEffect } from 'react';
-import { getContestList } from 'data-center';
+import { getContestList, getMemberInfo } from 'data-center';
+import Cookies from 'js-cookie';
 import { createContestListGlobalStore } from './contestListGlobalStore';
+import { useUserStore } from './userStore';
+import { useNotificationStore } from './notificationStore';
 
 function ContestStoreHandler() {
+    const setUserInfo = useUserStore.use.setUserInfo();
+    const setIsLogin = useUserStore.use.setIsLogin();
+    const setToken = useUserStore.use.setToken();
+    const isCookieExist = Cookies.get('access');
+    const setNotificationVisible = useNotificationStore.use.setIsVisible();
+
     useEffect(() => {
         const fetchData = async () => {
             const timestamp = Math.floor(Date.now() / 1000);
@@ -13,7 +22,22 @@ function ContestStoreHandler() {
             }
         };
 
+        const getUserInfo = async () => {
+            const res = await getMemberInfo();
+
+            if (res.success && isCookieExist) {
+                setUserInfo(res.data);
+                setIsLogin(true);
+                setToken(isCookieExist);
+            } else {
+                setNotificationVisible('登陆已过期，请重新登陆', 'error');
+            }
+        };
+
         void fetchData();
+        if (isCookieExist) {
+            void getUserInfo();
+        }
     }, []);
 
     return null;
