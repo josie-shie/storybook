bc965e47e2ea09a6886b6afd4643d1fc900ba1dc
Josie <chiao-hsinhsieh@CHIAO-HSINdeMacBook-Pro.local>
2023-12-05 18:34:05 +0800

    feat(MyGuessMyPlans): wrap the span button using the Slick component

--- apps/site-03/app/userInfo/myGuess/myGuessMyPlans.tsx
+++ apps/site-03/app/userInfo/myGuess/myGuessMyPlans.tsx
@@ -1,6 +1,7 @@
 'use client';
 import type { Dispatch, SetStateAction } from 'react';
 import { useState } from 'react';
+import { Slick } from 'ui';
 import BettingPlan from './components/bettingPlan/bettingPlan';
 import style from './myGuess.module.scss';
 import { useMyGuessStore, type MyPlans } from './myGuessStore';
@@ -9,6 +10,24 @@ import NoData from '@/components/baseNoData/noData';
 interface MyGuessMyPlansProps {
     setShowFilter: Dispatch<SetStateAction<boolean>>;
 }
+
+const tabList = [
+    {
+        label: '全部',
+        href: '/userInfo/myGuess/?status=totale',
+        status: 'totale'
+    },
+    {
+        label: '让球',
+        href: '/userInfo/myGuess/?status=handicap',
+        status: 'handicap'
+    },
+    {
+        label: '大小',
+        href: '/userInfo/myGuess/?status=size',
+        status: 'size'
+    }
+];
 
 const planActiveMap = {
     totale: { dispaly: '全部', value: 'totale' },
@@ -24,6 +43,7 @@ function MyGuessMyPlans({ setShowFilter }: MyGuessMyPlansProps) {
     };
 
     const myPlansData = useMyGuessStore.use.myGuess().myPlans[planActiveTab as keyof MyPlans];
+    const slideActive = planActiveTab ? tabList.findIndex(tab => tab.status === planActiveTab) : 0;
 
     return (
         <>
@@ -38,25 +58,26 @@ function MyGuessMyPlans({ setShowFilter }: MyGuessMyPlansProps) {
                     查看纪录
                 </span>
             </div>
-            <div className={style.tab}>
-                {Object.entries(planActiveMap).map(([key, value]) => (
-                    <span
-                        className={planActiveTab === value.value ? style.active : ''}
-                        key={key}
-                        onClick={() => {
-                            handlePlanTabClick(value.value);
-                        }}
-                    >
-                        {value.dispaly}
-                    </span>
-                ))}
-            </div>
-            <div className={style.plan}>
-                {myPlansData.length > 0 ? (
-                    myPlansData.map(item => <BettingPlan key={item.id} rowData={item} />)
-                ) : (
-                    <NoData />
-                )}
+            <div className={style.btnTab}>
+                <Slick slideActive={slideActive} styling="button" tabs={tabList}>
+                    {Object.entries(planActiveMap).map(([key, value]) => (
+                        <span
+                            className={planActiveTab === value.value ? style.active : ''}
+                            key={key}
+                            onClick={() => {
+                                handlePlanTabClick(value.value);
+                            }}
+                        >
+                            {myPlansData.length > 0 ? (
+                                myPlansData.map(item => (
+                                    <BettingPlan key={item.id} rowData={item} />
+                                ))
+                            ) : (
+                                <NoData />
+                            )}
+                        </span>
+                    ))}
+                </Slick>
             </div>
         </>
     );
