a701be563232c01725278b09fe923b1aad5dceef
susanna <susanna@future8.com>
2023-12-05 23:12:25 +0800

    feat: update bigData record page data

--- apps/site-03/app/recommend/bigData/(list)/components/recordFilter/recordFilter.tsx
+++ apps/site-03/app/recommend/bigData/(list)/components/recordFilter/recordFilter.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import SearchRecord from '../searchRecord/searchRecord';
+import { useDiscSelectStore } from '../../discSelectStore';
 import style from './recordFilter.module.scss';
 import BottomDrawer from '@/components/drawer/bottomDrawer';
 
@@ -11,20 +13,46 @@ function RecordFilter({
     onOpen: () => void;
     onClose: () => void;
 }) {
+    const setRecordList = useDiscSelectStore.use.setRecordList();
+    const recordList = useDiscSelectStore.use.recordList();
+
+    useEffect(() => {
+        setRecordList([
+            {
+                recordId: 1,
+                recordTime: 1701771917,
+                handicap: 'home',
+                odds: '1',
+                overUnder: '1',
+                startDate: 1701771917,
+                endDate: 1701771917,
+                state: 0
+            },
+            {
+                recordId: 2,
+                recordTime: 1701771917,
+                handicap: 'away',
+                odds: '4+',
+                overUnder: '1',
+                startDate: 1701771917,
+                endDate: 1701771917,
+                state: 1
+            }
+        ]);
+    }, [setRecordList]);
+
     return (
         <BottomDrawer isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
             <div className={style.recordFilter}>
-                <SearchRecord loading={false} />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading />
-                <SearchRecord loading={false} />
+                {recordList.map((record, idx) => {
+                    return (
+                        <SearchRecord
+                            index={recordList.length - idx}
+                            key={record.recordId}
+                            recordData={record}
+                        />
+                    );
+                })}
             </div>
         </BottomDrawer>
     );
