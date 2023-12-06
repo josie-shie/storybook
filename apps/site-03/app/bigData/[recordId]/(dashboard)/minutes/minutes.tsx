728c9d118b5c630233e78644aec896a2837ac363
susanna <susanna@future8.com>
2023-12-06 16:36:41 +0800

    feat: update bigData 15minutes page

--- apps/site-03/app/bigData/[recordId]/(dashboard)/minutes/minutes.tsx
+++ apps/site-03/app/bigData/[recordId]/(dashboard)/minutes/minutes.tsx
@@ -2,142 +2,79 @@
 import { useState } from 'react';
 import FifteenMinutesChart from '../../components/fifteenMinutesChart/fifteenMinutesChart';
 import ContestDrawerList from '../components/contestDrawerList';
+import { useAnalyticsResultStore } from '../../analysisResultStore';
 import style from './minutes.module.scss';
 
+function TimeRangeTable({
+    label,
+    upper,
+    lower,
+    setShowList
+}: {
+    label: string;
+    upper: number;
+    lower: number;
+    setShowList: (isShow: boolean) => void;
+}) {
+    return (
+        <div className={style.tableContainer}>
+            <div className={style.header}>{label}</div>
+
+            <div className={style.cell}>
+                <span
+                    onClick={() => {
+                        setShowList(true);
+                    }}
+                >
+                    上 {upper}
+                </span>
+            </div>
+            <div className={`${style.cell} ${style.odd}`}>
+                <span
+                    onClick={() => {
+                        setShowList(true);
+                    }}
+                >
+                    下 {lower}
+                </span>
+            </div>
+        </div>
+    );
+}
+
 function Minutes() {
+    const headers = [
+        '開場-14:59',
+        '15:00-29:59',
+        '30:00-半場',
+        '下半場-59:59',
+        '60:00-74:59',
+        '75:00-全場'
+    ];
     const [showList, setShowList] = useState(false);
+    const analysisRecord = useAnalyticsResultStore.use.analysisResultData();
 
     return (
         <>
             <div className={style.minutes}>
-                <FifteenMinutesChart />
+                <FifteenMinutesChart headers={headers} minsGoalList={analysisRecord.minutesGoal} />
                 <div className={style.dot}>
                     <span className={style.big}>大</span>
                     <span className={style.small}>小</span>
                 </div>
             </div>
-            <div className={style.tableContainer}>
-                <div className={style.header}>開場-14:59</div>
-                <div className={style.header}>15:00-29:59</div>
-                <div className={style.header}>30:00-半場</div>
-
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
+            <div className={style.contaniner}>
+                {headers.map((time, index) => (
+                    <TimeRangeTable
+                        key={time}
+                        label={time}
+                        lower={analysisRecord.minutesGoal[index].goalLower.length}
+                        setShowList={setShowList}
+                        upper={analysisRecord.minutesGoal[index].goalUpper.length}
+                    />
+                ))}
             </div>
-            <div className={style.tableContainer}>
-                <div className={style.header}>開場-14:59</div>
-                <div className={style.header}>15:00-29:59</div>
-                <div className={style.header}>30:00-半場</div>
 
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        上 40
-                    </span>
-                </div>
-
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
-                <div className={`${style.cell} ${style.odd}`}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        下 40
-                    </span>
-                </div>
-            </div>
             <ContestDrawerList
                 isOpen={showList}
                 onClose={() => {
