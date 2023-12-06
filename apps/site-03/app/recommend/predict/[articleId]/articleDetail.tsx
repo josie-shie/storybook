0a2923117ba5a4ac77d562e09fceb44ace8c0c79
Jelly <jelly@befuture8.com>
2023-12-06 15:05:22 +0800

    feat(predict): edit real api response parameter key and value

--- apps/site-03/app/recommend/predict/[articleId]/articleDetail.tsx
+++ apps/site-03/app/recommend/predict/[articleId]/articleDetail.tsx
@@ -17,32 +17,39 @@ function MasterDetail({ params }: { params: { articleId: string } }) {
 
     creatArticleStore({
         articleDetail: {
-            id: 1,
-            master: {
-                id: 0,
-                avatar: '',
-                name: '老梁聊球',
-                hotStreak: 9,
-                ranking: 10,
-                followed: true,
-                unlockNumber: 1000,
-                fansNumber: 34713
-            },
-            postTime: 1701679456,
-            title: '格鲁吉亚vs西班牙，来看我的精心推荐吧',
-            leagueName: '歐錦U20A',
-            dateTime: 1701679456,
-            homeTeamLogo: '',
-            homeTeamName: '泰国国立法政大学',
-            awayTeamLogo: '',
-            awayTeamName: '北曼谷学院',
-            content:
+            mentorId: 229,
+            mentorName: 'Josie的羅囉',
+            mentorImage:
+                'https://twtest8.s3.ap-northeast-1.amazonaws.com/a6e6524457f4778d83cecbdb7892df7b8170cb830a76d29470958221e78078ad.png',
+            analysisTitle: '格鲁吉亚vs西班牙，来看我的精心推荐吧',
+            analysisContent:
                 '【推荐分析】赛事前瞻：乌兰巴托FC主队近5场3胜1平1负，台中未来客队近5场2胜2平1负，谁能更胜一筹。',
-            unlock: false,
-            homeHandicap: 0.5,
-            awayHandicap: -0.5,
-            guessResult: 'none',
-            masterGuess: 'home'
+            homeTeam: {
+                id: 2670,
+                name: '泰国国立法政大学',
+                logo: '0'
+            },
+            awayTeam: {
+                id: 5397,
+                name: '北曼谷学院',
+                logo: '0'
+            },
+            matchTime: 1698667200,
+            createdAt: 1698659314,
+            leagueName: '女欧U19',
+            predictionResult: 'NONE',
+            playType: 'HOMEAWAY',
+            odds: {
+                handicap: -1.75,
+                overUnder: 3
+            },
+            fansNumber: 34713,
+            unlockNumber: 1000,
+            hotStreak: 9,
+            ranking: 10,
+            followed: true,
+            predictedPlay: 'LOCK',
+            price: 10
         },
         recommendationList: [
             {
