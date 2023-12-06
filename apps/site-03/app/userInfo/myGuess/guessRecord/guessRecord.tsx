ad02fcd7bf4780b7507b4ad251c96f58b0569064
Josie <chiao-hsinhsieh@CHIAO-HSINdeMacBook-Pro.local>
2023-12-06 18:22:07 +0800

    feat(GuessRecord): render the guessRecordList and use the isOpen state to toggle  the Drawer

--- apps/site-03/app/userInfo/myGuess/guessRecord/guessRecord.tsx
+++ apps/site-03/app/userInfo/myGuess/guessRecord/guessRecord.tsx
@@ -1,74 +1,36 @@
 import RecordCard from '../components/recordCard/recordCard';
+import { useMyGuessStore } from '../myGuessStore';
 import style from './guessRecord.module.scss';
-import { creatGuessRecordListStore, useGuessRecordListStore } from './guessRecordStore';
 import BottomDrawer from '@/components/drawer/bottomDrawer';
 
 function RecordContent() {
-    const recordList = useGuessRecordListStore.use.guessRecordList();
+    const guessRecordList = useMyGuessStore.use.myGuess().guessRecordList;
+
     return (
         <div className={style.guessRecord}>
             <div className={style.title}>
                 <span>競猜瀏覽紀錄</span>
             </div>
-            {recordList.map(item => (
+            {guessRecordList.map(item => (
                 <RecordCard key={item.id} recordItem={item} />
             ))}
         </div>
     );
 }
 
-function GuessRecord({
-    isOpen,
-    onOpen,
-    onClose
-}: {
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
-}) {
-    creatGuessRecordListStore({
-        isOpen: false,
-        guessRecordList: [
-            {
-                id: 7,
-                avatar: '',
-                name: '老不休聊球',
-                hotStreak: 2,
-                homeTeam: '欧锦U20A',
-                awayTeam: '斯洛文尼亚U20',
-                history: ['win', 'lose', 'draw', 'win', 'lose', 'draw', 'win', 'lose', 'draw'],
-                guess: 'big',
-                result: 'win',
-                guessValue: 0.5
-            },
-            {
-                id: 7,
-                avatar: '',
-                name: '老梁聊球',
-                hotStreak: 2,
-                homeTeam: '欧锦2A',
-                awayTeam: '斯洛文尼亚U0',
-                history: ['win', 'lose', 'draw', 'win', 'lose', 'draw', 'win', 'lose', 'draw'],
-                guess: 'home',
-                result: 'lose',
-                guessValue: 0.5
-            },
-            {
-                id: 7,
-                avatar: '',
-                name: '老張聊球',
-                hotStreak: 2,
-                homeTeam: '欧锦U200A',
-                awayTeam: '斯洛文尼亚U300',
-                history: ['win', 'lose', 'draw', 'win', 'lose', 'draw', 'win', 'lose', 'draw'],
-                guess: 'away',
-                result: 'draw',
-                guessValue: 0.5
-            }
-        ]
-    });
+function GuessRecord() {
+    const isOpenRecord = useMyGuessStore.use.myGuess().isOpen;
+    const setIsOpenRecord = useMyGuessStore.use.setOpen();
     return (
-        <BottomDrawer isOpen={isOpen} onClose={onClose} onOpen={onOpen}>
+        <BottomDrawer
+            isOpen={isOpenRecord}
+            onClose={() => {
+                setIsOpenRecord(false);
+            }}
+            onOpen={() => {
+                setIsOpenRecord(true);
+            }}
+        >
             <RecordContent />
         </BottomDrawer>
     );
--- apps/site-03/app/userInfo/myGuess/myGuessMyPlans.tsx
+++ apps/site-03/app/userInfo/myGuess/myGuessMyPlans.tsx
@@ -1,15 +1,10 @@
 'use client';
-import type { Dispatch, SetStateAction } from 'react';
 import { useState } from 'react';
 import { Slick } from 'ui';
 import BettingPlan from './components/bettingPlan/bettingPlan';
 import style from './myGuess.module.scss';
-import { useMyGuessStore, type MyPlans } from './myGuessStore';
+import { useMyGuessStore, type MyPlans, type RecordItem } from './myGuessStore';
 import NoData from '@/components/baseNoData/noData';
-
-interface MyGuessMyPlansProps {
-    setShowFilter: Dispatch<SetStateAction<boolean>>;
-}
 
 const tabList = [
     {
@@ -35,11 +30,74 @@ const planActiveMap = {
     size: { dispaly: '大小', value: 'size' }
 };
 
-function MyGuessMyPlans({ setShowFilter }: MyGuessMyPlansProps) {
+function MyGuessMyPlans() {
     const [planActiveTab, setPlanActiveTab] = useState(planActiveMap.totale.value);
+    const openRecord = useMyGuessStore.use.setOpen();
+    const setGuessRecordList = useMyGuessStore.use.setGuessRecordList();
 
     const handlePlanTabClick = (tabName: string) => {
         setPlanActiveTab(tabName);
+    };
+
+    const fetchListData = () => {
+        return [
+            {
+                id: 7,
+                avatar: '',
+                name: '羅曼琉球',
+                hotStreak: 2,
+                homeTeam: '欧锦U20A',
+                awayTeam: '斯洛文尼亚U20',
+                history: ['win', 'lose', 'draw', 'win', 'lose', 'draw', 'win', 'lose', 'draw'],
+                guess: 'home',
+                result: 'win',
+                guessValue: 0.5
+            },
+            {
+                id: 10,
+                avatar: '',
+                name: '小羅聊球',
+                hotStreak: 2,
+                homeTeam: '欧锦U20A',
+                awayTeam: '斯洛文尼亚U20',
+                history: ['win', 'win', 'win', 'win', 'lose', 'draw', 'win', 'win', 'draw'],
+                guess: 'big',
+                result: 'lose',
+                guessValue: 0.5
+            },
+            {
+                id: 10,
+                avatar: '',
+                name: '小羅聊球',
+                hotStreak: 2,
+                homeTeam: '欧锦U20A',
+                awayTeam: '斯洛文尼亚U20',
+                history: ['win', 'win', 'draw', 'win', 'win', 'draw', 'win', 'win', 'draw'],
+                guess: 'home',
+                result: 'draw',
+                guessValue: 0.5
+            },
+            {
+                id: 10,
+                avatar: '',
+                name: '小羅聊球',
+                hotStreak: 2,
+                homeTeam: '欧锦U20A',
+                awayTeam: '斯洛文尼亚U20',
+                history: ['win', 'win', 'draw', 'win', 'win', 'draw', 'win', 'win', 'draw'],
+                guess: 'small',
+                result: 'draw',
+                guessValue: 0.5
+            }
+        ];
+    };
+
+    const handleOpenRecord = () => {
+        const list = fetchListData();
+        setGuessRecordList(list as RecordItem[]);
+        setTimeout(() => {
+            openRecord(true);
+        }, 0);
     };
 
     const myPlansData = useMyGuessStore.use.myGuess().myPlans[planActiveTab as keyof MyPlans];
@@ -49,12 +107,7 @@ function MyGuessMyPlans({ setShowFilter }: MyGuessMyPlansProps) {
         <>
             <div className={style.title}>
                 <span>我猜过的</span>
-                <span
-                    className={style.record}
-                    onClick={() => {
-                        setShowFilter(true);
-                    }}
-                >
+                <span className={style.record} onClick={handleOpenRecord}>
                     查看纪录
                 </span>
             </div>
