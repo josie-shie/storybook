1971e6ae34e86bf316a0874b44a9bf17017e3424
susanna <susanna@future8.com>
2023-12-06 16:55:55 +0800

    feat: update bigData goalRange page

--- apps/site-03/app/bigData/[recordId]/(dashboard)/range/range.tsx
+++ apps/site-03/app/bigData/[recordId]/(dashboard)/range/range.tsx
@@ -2,64 +2,100 @@
 import { useState } from 'react';
 import GoalRangeChart from '../../components/goalRangeChart/goalRangeChart';
 import ContestDrawerList from '../components/contestDrawerList';
+import { useAnalyticsResultStore } from '../../analysisResultStore';
 import style from './range.module.scss';
+
+function TableCell({
+    cellValue,
+    setShowList
+}: {
+    cellValue: number[];
+    setShowList: (isShow: boolean) => void;
+}) {
+    const setQueryMatchList = useAnalyticsResultStore.use.setQueryMatchList();
+
+    const openMatchListDrawer = (matchIdsList: number[]) => {
+        setQueryMatchList(matchIdsList);
+        setShowList(true);
+    };
+
+    return (
+        <div className={`${style.cell}`}>
+            <span
+                onClick={() => {
+                    openMatchListDrawer(cellValue);
+                }}
+            >
+                {cellValue.length}
+            </span>
+        </div>
+    );
+}
 
 function Range() {
     const [showList, setShowList] = useState(false);
+    const analysisRecord = useAnalyticsResultStore.use.analysisResultData();
+    const headers = [
+        {
+            class: style.first,
+            label: '0-1',
+            value: analysisRecord.goalRange.goalRange0To1.length,
+            color: '#6357F0'
+        },
+        {
+            class: style.second,
+            label: '2-3',
+            value: analysisRecord.goalRange.goalRange2To3.length,
+            color: '#33AD1F'
+        },
+        {
+            class: style.three,
+            label: '4-6',
+            value: analysisRecord.goalRange.goalRange4To6.length,
+            color: '#4489FF'
+        },
+        {
+            class: style.four,
+            label: '7以上',
+            value: analysisRecord.goalRange.goalRange7Upper.length,
+            color: '#FBB03B'
+        }
+    ];
 
     return (
         <>
             <div className={style.range}>
-                <GoalRangeChart />
+                <GoalRangeChart chartList={headers} />
                 <div className={style.dot}>
-                    <span className={style.first}>0-1</span>
-                    <span className={style.second}>2-3</span>
-                    <span className={style.three}>4-6</span>
-                    <span className={style.four}>7以上</span>
+                    {headers.map(header => (
+                        <span className={header.class} key={header.label}>
+                            {header.label}
+                        </span>
+                    ))}
                 </div>
             </div>
             <div className={style.tableContainer}>
-                <div className={style.header}>0-1</div>
-                <div className={style.header}>2-3</div>
-                <div className={style.header}>4-6</div>
-                <div className={style.header}>7以上</div>
-
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        55
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        32
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        18
-                    </span>
-                </div>
-                <div className={style.cell}>
-                    <span
-                        onClick={() => {
-                            setShowList(true);
-                        }}
-                    >
-                        11
-                    </span>
-                </div>
+                {headers.map(header => (
+                    <div className={style.header} key={header.label}>
+                        {header.label}
+                    </div>
+                ))}
+                <TableCell
+                    cellValue={analysisRecord.goalRange.goalRange0To1}
+                    setShowList={setShowList}
+                />
+                <TableCell
+                    cellValue={analysisRecord.goalRange.goalRange2To3}
+                    setShowList={setShowList}
+                />
+                <TableCell
+                    cellValue={analysisRecord.goalRange.goalRange4To6}
+                    setShowList={setShowList}
+                />
+                <TableCell
+                    cellValue={analysisRecord.goalRange.goalRange7Upper}
+                    setShowList={setShowList}
+                />
             </div>
             <ContestDrawerList
                 isOpen={showList}
--- apps/site-03/app/bigData/[recordId]/components/goalRangeChart/goalRangeChart.tsx
+++ apps/site-03/app/bigData/[recordId]/components/goalRangeChart/goalRangeChart.tsx
@@ -1,52 +1,43 @@
 import ReactEcharts from 'echarts-for-react';
 import style from './goalRangeChart.module.scss';
 
-const options = {
-    tooltip: {
-        trigger: 'item'
-    },
-    legend: {
-        show: false,
-        left: 'left'
-    },
-    series: [
-        {
-            name: '進球數區間',
-            type: 'pie',
-            radius: ['35%', '65%'],
-            data: [
-                {
-                    value: 248,
-                    name: '7以上',
-                    itemStyle: { borderWidth: 2, borderColor: '#fff', color: '#FBB03B' }
-                },
-                {
-                    value: 835,
-                    name: '4-6',
-                    itemStyle: { borderWidth: 2, borderColor: '#fff', color: '#4489FF' }
-                },
-                {
-                    value: 780,
-                    name: '2-3',
-                    itemStyle: { borderWidth: 2, borderColor: '#fff', color: '#33AD1F' }
-                },
-                {
-                    value: 1084,
-                    name: '0-1',
-                    itemStyle: { borderWidth: 2, borderColor: '#fff', color: '#6357F0' }
-                }
-            ],
-            label: {
-                distanceToLabelLine: -10
-            },
-            labelLine: {
-                show: false
-            }
-        }
-    ]
-};
+interface ChartType {
+    class: string;
+    label: string;
+    value: number;
+    color: string;
+}
 
-function GoalRangeChart() {
+function GoalRangeChart({ chartList }: { chartList: ChartType[] }) {
+    const seriesList = chartList.map(item => ({
+        value: item.value,
+        name: item.label,
+        itemStyle: { borderWidth: 2, borderColor: '#fff', color: item.color }
+    }));
+
+    const options = {
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            show: false,
+            left: 'left'
+        },
+        series: [
+            {
+                name: '進球數區間',
+                type: 'pie',
+                radius: ['35%', '65%'],
+                data: seriesList,
+                label: {
+                    distanceToLabelLine: -10
+                },
+                labelLine: {
+                    show: false
+                }
+            }
+        ]
+    };
     return (
         <div className={style.goalRangeChart}>
             <div className={style.chartContainer}>
