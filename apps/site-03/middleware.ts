382354ebad7097b31b4d49d4a762f2a5a57a5369
Peter <peter>
2023-12-07 11:30:33 +0800

    feat(permission): middleware check access to protect route

---
+++ apps/site-03/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.has('access');
+    if (!token) return NextResponse.redirect(new URL('/', request.url));
+}
+
+export const config = {
+    matcher: '/userInfo/:path*'
+};
