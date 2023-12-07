87be6ea5adc8006639f7d94cb7ba3a310f0861f2
Peter <peter>
2023-12-07 14:01:51 +0800

    feat(middleware): redirect to login

--- apps/site-03/middleware.ts
+++ apps/site-03/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.has('access');
-    if (!token) return NextResponse.redirect(new URL('/', request.url));
+    if (!token) return NextResponse.redirect(new URL('/?auth=login', request.url));
 }
 
 export const config = {
