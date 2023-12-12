284fae620eefdc0d65c3ba6d993cbaab8cb394ca
Ben <ben@befuture8.com>
2023-12-12 11:02:54 +0800

    feat: get unlock list query type

--- packages/data-center/src/user/graphqlQueries.ts
+++ packages/data-center/src/user/graphqlQueries.ts
@@ -122,11 +122,43 @@ export const GET_UNLOCKED_QUERY = `
                 awayTeamName
                 matchTime
                 createdAt
-                predictStat
                 memberTags {
-                    memberId
-                    type
-                    ranking
+                    id
+                    tagName
+                    note
+                    colorCode
+                    weekHitRecentTen
+                    weekMaxAccurateStreak
+                    weekHitMatches
+                    weekTotalMatches
+                    weekHitRate
+                    weekHitRateDisplay
+                    weekRanking
+                    weekHistoryMaxWinStreak
+                    monthHitRecentTen
+                    monthMaxAccurateStreak
+                    monthHitMatches
+                    monthTotalMatches
+                    monthHitRate
+                    monthHitRateDisplay
+                    monthRanking
+                    monthHistoryMaxWinStreak
+                    quarterHitRecentTen
+                    quarterMaxAccurateStreak
+                    quarterHitMatches
+                    quarterTotalMatches
+                    quarterHitRate
+                    quarterHitRateDisplay
+                    quarterRanking
+                    quarterHistoryMaxWinStreak
+                    winHitRecentTen
+                    winMaxAccurateStreak
+                    winHitMatches
+                    winTotalMatches
+                    winHitRate
+                    winHitRateDisplay
+                    winRanking
+                    winHistoryMaxWinStreak
                 }  
             }
         }
