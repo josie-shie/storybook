00a62258a2a296aaf840d28379fa4ceb92ae6428
Jelly <jelly@befuture8.com>
2023-12-13 14:03:12 +0800

    feat: add article detail

---
+++ apps/site-03/app/master/article/[articleId]/page.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { getPostDetail } from 'data-center';
+import ArticleDetail from './articleDetail';
+import Footer from '@/components/footer/footer';
+
+export const metadata: Metadata = {
+    title: '专家预测'
+};
+
+async function Page({ params }: { params: { articleId: string } }) {
+    const articleDetail = await getPostDetail({ postId: Number(params.articleId) });
+
+    if (!articleDetail.success) {
+        return new Error();
+    }
+
+    return (
+        <>
+            <ArticleDetail articleDetail={articleDetail.data} params={params} />
+            <Footer />
+        </>
+    );
+}
+
+export default Page;
