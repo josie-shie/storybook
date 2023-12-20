767ea38e9d7f2e82c9fbb81bc9ca6f79d0a8db32
susanna <susanna@future8.com>
2023-12-20 14:38:29 +0800

    chore: update bigData syntax

--- apps/site-03/app/bigData/(list)/handicapAnalysisForm.tsx
+++ apps/site-03/app/bigData/(list)/handicapAnalysisForm.tsx
@@ -241,11 +241,11 @@ function HandicapAnalysisForm() {
         }
     }, [dialogErrorType, setDialogContent]);
 
-    const submit = async () => {
+    const submit = () => {
         if (!isVip) {
             setOpenDialog(true);
         } else {
-            await getTrendAnalysis(startDate, endDate);
+            void getTrendAnalysis(startDate, endDate);
         }
     };
 
@@ -270,9 +270,7 @@ function HandicapAnalysisForm() {
             <motion.button
                 className={`${style.search} ${!startDate || !endDate ? style.disableButton : ''}`}
                 disabled={!startDate || !endDate}
-                onClick={async () => {
-                    await submit();
-                }}
+                onClick={submit}
                 type="button"
                 whileTap={{ scale: 0.9 }}
             >
