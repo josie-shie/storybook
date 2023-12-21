c0a6c0194eea41a409911b83e6700735bd41de49
Jelly <jelly@befuture8.com>
2023-12-21 17:59:39 +0800

    feat: top border radius

--- apps/site-03/app/master/article/[articleId]/info.tsx
+++ apps/site-03/app/master/article/[articleId]/info.tsx
@@ -45,69 +45,72 @@ function Info({ article, setArticle }: InfoProps) {
     };
 
     return (
-        <section className={style.info}>
-            <div className={style.detail}>
+        <>
+            <section className={style.info}>
+                <div className={style.detail}>
+                    <div
+                        onClick={() => {
+                            goMasterPredict(article.mentorId);
+                        }}
+                    >
+                        <Avatar borderColor="#fff" size={54} src={article.mentorImage} />
+                    </div>
+                    <div className={style.content}>
+                        <div className={style.top}>
+                            <span className={style.name}>{article.mentorName}</span>
+                        </div>
+                        <div>
+                            {article.tag.winMaxAccurateStreak > 0 && (
+                                <Tag
+                                    icon={<Image alt="fire" src={Fire} />}
+                                    text={`${article.tag.winMaxAccurateStreak} 連紅`}
+                                />
+                            )}
+
+                            {article.tag.quarterRanking > 0 && (
+                                <Tag
+                                    background="#fff"
+                                    color="#4489ff"
+                                    text={`季榜 ${article.tag.quarterRanking}`}
+                                />
+                            )}
+                            {article.tag.monthRanking > 0 && (
+                                <Tag
+                                    background="#fff"
+                                    color="#4489ff"
+                                    text={`月榜 ${article.tag.monthRanking}`}
+                                />
+                            )}
+                            {article.tag.weekRanking > 0 && (
+                                <Tag
+                                    background="#fff"
+                                    color="#4489ff"
+                                    text={`周榜 ${article.tag.weekRanking}`}
+                                />
+                            )}
+                        </div>
+                        <div className={style.bottom}>
+                            {article.fansNumber > 0 && <span>粉絲: {article.fansNumber} </span>}
+                            {article.unlockNumber > 0 && <span>解鎖: {article.unlockNumber} </span>}
+                            {article.tag.quarterHitRate > 0 && (
+                                <span>
+                                    近一季猜球胜率: {Math.round(article.tag.quarterHitRate * 100)}%
+                                </span>
+                            )}
+                        </div>
+                    </div>
+                </div>
                 <div
+                    className={article.followed ? style.focused : style.focus}
                     onClick={() => {
-                        goMasterPredict(article.mentorId);
+                        void onIsFocused(article.mentorId, article.followed);
                     }}
                 >
-                    <Avatar borderColor="#fff" size={54} src={article.mentorImage} />
+                    {article.followed ? '已关注' : '关注'}
                 </div>
-                <div className={style.content}>
-                    <div className={style.top}>
-                        <span className={style.name}>{article.mentorName}</span>
-                    </div>
-                    <div>
-                        {article.tag.winMaxAccurateStreak > 0 && (
-                            <Tag
-                                icon={<Image alt="fire" src={Fire} />}
-                                text={`${article.tag.winMaxAccurateStreak} 連紅`}
-                            />
-                        )}
-
-                        {article.tag.quarterRanking > 0 && (
-                            <Tag
-                                background="#fff"
-                                color="#4489ff"
-                                text={`季榜 ${article.tag.quarterRanking}`}
-                            />
-                        )}
-                        {article.tag.monthRanking > 0 && (
-                            <Tag
-                                background="#fff"
-                                color="#4489ff"
-                                text={`月榜 ${article.tag.monthRanking}`}
-                            />
-                        )}
-                        {article.tag.weekRanking > 0 && (
-                            <Tag
-                                background="#fff"
-                                color="#4489ff"
-                                text={`周榜 ${article.tag.weekRanking}`}
-                            />
-                        )}
-                    </div>
-                    <div className={style.bottom}>
-                        {article.fansNumber > 0 && <span>粉絲: {article.fansNumber} </span>}
-                        {article.unlockNumber > 0 && <span>解鎖: {article.unlockNumber} </span>}
-                        {article.tag.quarterHitRate > 0 && (
-                            <span>
-                                近一季猜球胜率: {Math.round(article.tag.quarterHitRate * 100)}%
-                            </span>
-                        )}
-                    </div>
-                </div>
-            </div>
-            <div
-                className={article.followed ? style.focused : style.focus}
-                onClick={() => {
-                    void onIsFocused(article.mentorId, article.followed);
-                }}
-            >
-                {article.followed ? '已关注' : '关注'}
-            </div>
-        </section>
+            </section>
+            <div className={style.radius} />
+        </>
     );
 }
 
