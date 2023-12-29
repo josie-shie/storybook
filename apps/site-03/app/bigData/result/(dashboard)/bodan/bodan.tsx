f6442a169006827dc83388d34fc00b075a76b4a1
susanna <susanna@future8.com>
2023-12-29 17:25:10 +0800

    fix: fix bigData bodan data process

--- apps/site-03/app/bigData/result/(dashboard)/bodan/bodan.tsx
+++ apps/site-03/app/bigData/result/(dashboard)/bodan/bodan.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getFootballStatsMatches } from 'data-center';
 import { useNotificationStore } from '@/app/notificationStore';
 import { useAnalyticsResultStore } from '../../analysisResultStore';
@@ -19,40 +19,7 @@ function Bodan() {
     const setShowContestDrawer = useAnalyticsResultStore.use.setShowContestDrawer();
     const setSelectedResult = useAnalyticsResultStore.use.setSelectedResult();
     const setMatchList = useAnalyticsResultStore.use.setContestList();
-    const scoreListMapping: ScoreMapping = {};
-
-    const scores = [
-        { score: '1-0', value: scoreListMapping['1-0'] || [] },
-        { score: '0-1', value: scoreListMapping['0-1'] || [] },
-        { score: '0-0', value: scoreListMapping['0-0'] || [] },
-        { score: '2-0', value: scoreListMapping['2-0'] || [] },
-        { score: '0-2', value: scoreListMapping['0-2'] || [] },
-        { score: '1-1', value: scoreListMapping['1-1'] || [] },
-        { score: '2-1', value: scoreListMapping['2-1'] || [] },
-        { score: '1-2', value: scoreListMapping['1-2'] || [] },
-        { score: '2-2', value: scoreListMapping['2-2'] || [] },
-        { score: '3-0', value: scoreListMapping['3-0'] || [] },
-        { score: '0-3', value: scoreListMapping['0-3'] || [] },
-        { score: '3-3', value: scoreListMapping['3-3'] || [] },
-        { score: '3-1', value: scoreListMapping['3-1'] || [] },
-        { score: '1-3', value: scoreListMapping['1-3'] || [] },
-        { score: '4-4', value: scoreListMapping['4-4'] || [] },
-        { score: '3-2', value: scoreListMapping['3-2'] || [] },
-        { score: '2-3', value: scoreListMapping['2-3'] || [] },
-        { score: '其他', value: scoreListMapping['其他'] || [] },
-        { score: '4-0', value: scoreListMapping['4-0'] || [] },
-        { score: '0-4', value: scoreListMapping['0-4'] || [] },
-        { score: '', value: [] },
-        { score: '4-1', value: scoreListMapping['4-1'] || [] },
-        { score: '1-4', value: scoreListMapping['1-4'] || [] },
-        { score: '', value: [] },
-        { score: '4-2', value: scoreListMapping['4-2'] || [] },
-        { score: '2-4', value: scoreListMapping['2-4'] || [] },
-        { score: '', value: [] },
-        { score: '4-3', value: scoreListMapping['4-3'] || [] },
-        { score: '3-4', value: scoreListMapping['3-4'] || [] },
-        { score: '', value: [] }
-    ];
+    const [scoreListMapping, setScoreListMapping] = useState<ScoreMapping>({});
 
     const fetchMatchList = async (matchIdList: number[]) => {
         const res = await getFootballStatsMatches({ matchIds: matchIdList });
@@ -87,10 +54,45 @@ function Bodan() {
     }, [contestInfo, setFilterInit]);
 
     useEffect(() => {
+        const newScoreListMapping: ScoreMapping = {};
         analysisRecord?.correctScores?.forEach(item => {
-            scoreListMapping[item.score] = item.matches;
+            newScoreListMapping[item.score] = item.matches;
         });
+        setScoreListMapping(newScoreListMapping);
     }, [analysisRecord]);
+
+    const scores = [
+        { score: '1-0', value: scoreListMapping['1-0'] || [] },
+        { score: '0-1', value: scoreListMapping['0-1'] || [] },
+        { score: '0-0', value: scoreListMapping['0-0'] || [] },
+        { score: '2-0', value: scoreListMapping['2-0'] || [] },
+        { score: '0-2', value: scoreListMapping['0-2'] || [] },
+        { score: '1-1', value: scoreListMapping['1-1'] || [] },
+        { score: '2-1', value: scoreListMapping['2-1'] || [] },
+        { score: '1-2', value: scoreListMapping['1-2'] || [] },
+        { score: '2-2', value: scoreListMapping['2-2'] || [] },
+        { score: '3-0', value: scoreListMapping['3-0'] || [] },
+        { score: '0-3', value: scoreListMapping['0-3'] || [] },
+        { score: '3-3', value: scoreListMapping['3-3'] || [] },
+        { score: '3-1', value: scoreListMapping['3-1'] || [] },
+        { score: '1-3', value: scoreListMapping['1-3'] || [] },
+        { score: '4-4', value: scoreListMapping['4-4'] || [] },
+        { score: '3-2', value: scoreListMapping['3-2'] || [] },
+        { score: '2-3', value: scoreListMapping['2-3'] || [] },
+        { score: '其他', value: scoreListMapping['其他'] || [] },
+        { score: '4-0', value: scoreListMapping['4-0'] || [] },
+        { score: '0-4', value: scoreListMapping['0-4'] || [] },
+        { score: '', value: [] },
+        { score: '4-1', value: scoreListMapping['4-1'] || [] },
+        { score: '1-4', value: scoreListMapping['1-4'] || [] },
+        { score: '', value: [] },
+        { score: '4-2', value: scoreListMapping['4-2'] || [] },
+        { score: '2-4', value: scoreListMapping['2-4'] || [] },
+        { score: '', value: [] },
+        { score: '4-3', value: scoreListMapping['4-3'] || [] },
+        { score: '3-4', value: scoreListMapping['3-4'] || [] },
+        { score: '', value: [] }
+    ];
 
     return (
         <div className={style.bodan}>
