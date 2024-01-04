ce60a4e7b92c060aadc97753956b96b34bcd38c7
Johnny <johnny@befuture8.com>
2024-01-04 15:32:48 +0800

    feat: error rename

--- apps/site-03/app/error.tsx
+++ apps/site-03/app/error.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Header from '@/components/header/headerLogo';
 import Footer from '@/components/footer/footer';
 import style from './error.module.scss';
-import ErrorIMage from './img/error.jpg';
+import ErrorImage from './img/error.jpg';
 
 export default function Error({ error }: { error: Error & { digest?: string } }) {
     useEffect(() => {
@@ -18,7 +18,7 @@ export default function Error({ error }: { error: Error & { digest?: string } })
             <Header />
             <div className={style.box}>
                 <div className={style.content}>
-                    <Image alt="error" height={100} src={ErrorIMage} width={100} />
+                    <Image alt="error" height={100} src={ErrorImage} width={100} />
                     <p className={style.text}>哎呀，系统暂时出错！请稍候重试</p>
                 </div>
             </div>
