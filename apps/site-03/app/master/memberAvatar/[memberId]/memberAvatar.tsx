38a04d084c99e0d7f3e2a91332dcbcad8f68b35d
Jelly <jelly@befuture8.com>
2024-01-05 10:46:33 +0800

    feat(memberAvatar): change tab to slackPro tab

--- apps/site-03/app/master/memberAvatar/[memberId]/memberAvatar.tsx
+++ apps/site-03/app/master/memberAvatar/[memberId]/memberAvatar.tsx
@@ -1,12 +1,65 @@
-import InfoTabs from './infoTabs';
+'use client';
+import type { Metadata } from 'next';
 import style from './memberAvatar.module.scss';
+import { Slick } from 'ui/stories/slickPro/slick';
+import Guess from './components/guess/guess';
+import MemberItem from './components/memberItem/memberItem';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-function MemberAvatar({ params }: { params: { memberId: string } }) {
+export const metadata: Metadata = {
+    title: '专家预测 | FutureSport'
+};
+
+function MasterAvatar({ params }: { params: { memberId: string } }) {
+    const [secondRender, setSecondRender] = useState(false);
+
+    const searchParams = useSearchParams();
+    const status = searchParams.get('status');
+
+    const tabList = [
+        {
+            label: '猜球',
+            href: `/master/memberAvatar/${params.memberId}?status=guess`,
+            status: 'guess'
+        },
+        {
+            label: '关注',
+            href: `/master/memberAvatar/${params.memberId}?status=focus`,
+            status: 'focus'
+        }
+    ];
+
+    const initialSlide = status ? tabList.findIndex(tab => tab.status === status) : 0;
+
+    const onSlickEnd = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    useEffect(() => {
+        setSecondRender(true);
+    }, []);
     return (
         <div className={style.masterAvatar}>
-            <InfoTabs params={params} />
+            <Slick
+                className={style.slick}
+                initialSlide={initialSlide}
+                onSlickEnd={onSlickEnd}
+                styling="button"
+                tabs={tabList}
+            >
+                <div className={`${style.largeGap}`}>
+                    {secondRender || status === 'guess' ? <Guess params={params} /> : null}
+                </div>
+                <div className={`${style.largeGap}`}>
+                    {secondRender || status === 'focus' ? <MemberItem params={params} /> : null}
+                </div>
+            </Slick>
         </div>
     );
 }
 
-export default MemberAvatar;
+export default MasterAvatar;
