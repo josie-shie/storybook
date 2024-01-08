2f6d2cd2c6273b148610f2ff58e7e1bd450505fd
Josie <chiao-hsinhsieh@CHIAO-HSINdeMacBook-Pro.local>
2024-01-08 17:00:21 +0800

    feat(component): add ConfirmPayDrawer

---
+++ apps/site-03/components/confirmPayDrawer/confirmPayDrawer.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+import { Button } from '@mui/material';
+import BottomDrawer from '@/components/drawer/bottomDrawer';
+import { useUserStore } from '@/app/userStore';
+import style from './confirmPayDrawer.module.scss';
+import Star from './img/star.png';
+import PayHand from './img/payHand.png';
+
+interface ConfirmPayDrawerProps {
+    title?: string;
+    price: number;
+    isOpen: boolean;
+    onClose: () => void;
+    onOpen: () => void;
+    onPay: () => void;
+}
+
+function ConfirmPayDrawer({
+    isOpen,
+    onClose,
+    onOpen,
+    onPay,
+    price,
+    title = '解锁本场?'
+}: ConfirmPayDrawerProps) {
+    const userInfo = useUserStore.use.userInfo();
+
+    return (
+        <BottomDrawer
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpen={onOpen}
+            propsStyle={{
+                height: '200px',
+                backgroundColor: '#fff'
+            }}
+            topLineDisplay="none"
+        >
+            <div className={style.confirmPay}>
+                <div className={style.PayHandImg}>
+                    <Image alt="payHand" height={100} src={PayHand} width={100} />
+                </div>
+                <div className={style.payContent}>
+                    <div className={style.price}>
+                        <span className={style.text}>支付</span>
+                        <span className={style.number}>
+                            <Image alt="priceLogo" className={style.image} src={Star} width={14} />
+                            {price}
+                        </span>
+                    </div>
+                    <span className={style.text}>{title}</span>
+                </div>
+                <div className={style.balance}>
+                    我的餘額: {userInfo.balance ? userInfo.balance : 0}金幣
+                </div>
+                <Button className={style.payBtn} fullWidth onClick={onPay}>
+                    <Image
+                        alt="video_icon"
+                        className={style.icon}
+                        height={17}
+                        src={Star}
+                        width={17}
+                    />
+                    <span>支付</span>
+                </Button>
+            </div>
+        </BottomDrawer>
+    );
+}
+
+export default ConfirmPayDrawer;
