a3fe1eff7ca29bb46a0a6bfa25ff977f6d78e85a
Zafer <zafer@befuture8.com>
2024-01-12 14:31:17 +0800

    feat: guess detail intercepting route

---
+++ apps/site-03/app/recommend/guess/(list)/@para/(.)detail/[matchId]/layout.tsx
@@ -0,0 +1,8 @@
+'use client';
+import GuessDetail from '@/app/recommend/guess/detail/[matchId]/guessDetail';
+
+function GuessDetailLayout() {
+    return <GuessDetail backHistory />;
+}
+
+export default GuessDetailLayout;
--- apps/site-03/app/recommend/guess/(list)/layout.tsx
+++ apps/site-03/app/recommend/guess/(list)/layout.tsx
@@ -1,11 +1,42 @@
 'use client';
 import type { ReactNode } from 'react';
+import { useParams } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
 import Header from '@/components/header/headerLogo';
 import Footer from '@/components/footer/footer';
 import style from './layout.module.scss';
 import { creatRankStore } from './rankStore';
 
-function ContestLayout({ children }: { children: ReactNode }) {
+function InterceptingDetail({ para }: { para: ReactNode }) {
+    const params = useParams();
+
+    return (
+        <AnimatePresence>
+            {params.matchId ? (
+                <motion.div
+                    animate={{ transform: 'translateX(0)' }}
+                    exit={{ transform: 'translateX(100%)' }}
+                    initial={{ transform: 'translateX(100%)' }}
+                    key="guessDetail"
+                    style={{
+                        background: '#fff',
+                        width: '100%',
+                        position: 'fixed',
+                        top: 0,
+                        zIndex: 1000,
+                        height: '100dvh',
+                        overflowY: 'auto'
+                    }}
+                    transition={{ duration: 0.2 }}
+                >
+                    {para}
+                </motion.div>
+            ) : null}
+        </AnimatePresence>
+    );
+}
+
+function ContestLayout({ children, para }: { children: ReactNode; para: ReactNode }) {
     creatRankStore({ weekRankList: [], monthRankList: [], seasonRankList: [], masterRankList: [] });
 
     return (
@@ -13,6 +44,7 @@ function ContestLayout({ children }: { children: ReactNode }) {
             <Header link="/recommend/guess" title="高手榜" />
             <div className={style.guess}>{children}</div>
             <Footer />
+            <InterceptingDetail para={para} />
         </>
     );
 }
