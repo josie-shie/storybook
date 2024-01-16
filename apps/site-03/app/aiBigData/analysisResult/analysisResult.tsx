624197548abc7f30229901d4743839822c59da9c
susanna <susanna@future8.com>
2024-01-16 17:13:43 +0800

    feat: add bigData intercept route

--- apps/site-03/app/aiBigData/analysisResult/analysisResult.tsx
+++ apps/site-03/app/aiBigData/analysisResult/analysisResult.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 function AnalysisResult() {
-    return <div>智能分析結果頁</div>;
+    return (
+        <div>
+            智能分析結果頁
+            <Link href="/football/4053571/analyze">去賽事</Link>
+        </div>
+    );
 }
 
 export default AnalysisResult;
