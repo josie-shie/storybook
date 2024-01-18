ec9de4f03f41db274543da3ef86ce48981a67cb1
susanna <susanna@future8.com>
2024-01-18 16:19:14 +0800

    feat: add bigData form submit button

--- apps/site-03/app/aiBigData/queryForm/queryForm.tsx
+++ apps/site-03/app/aiBigData/queryForm/queryForm.tsx
@@ -1,8 +1,55 @@
 'use client';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
+import { useUserStore } from '@/store/userStore';
+import { useQueryFormStore } from '../queryFormStore';
 import style from './queryForm.module.scss';
 import Tips from './components/tips/tips';
 import LeagueDrawer from './components/leagueDrawer/leagueDrawer';
 import Form from './components/form/form';
+import CoinIcon from './img/coin.svg';
+
+function SubmitButton() {
+    const router = useRouter();
+    const userInfo = useUserStore.use.userInfo();
+    const isVip = useUserStore.use.memberSubscribeStatus().planId; // 1是VIP
+    const setOpenNormalDialog = useQueryFormStore.use.setOpenNormalDialog();
+    const setIsOpenPayDrawer = useQueryFormStore.use.setIsOpenPayDrawer();
+    const setIsAnalysisBySearch = useQueryFormStore.use.setIsAnalysisBySearch();
+
+    const submit = () => {
+        if (!isVip) {
+            if (userInfo.balance < 80) {
+                setOpenNormalDialog(true);
+                return;
+            }
+            setIsOpenPayDrawer(true);
+            return;
+        }
+
+        setIsAnalysisBySearch(true);
+        router.push('/aiBigData/result');
+    };
+
+    return (
+        <div className={style.submit}>
+            <motion.button
+                className={style.search}
+                onClick={submit}
+                type="button"
+                whileTap={{ scale: 0.9 }}
+            >
+                {!isVip ? (
+                    <>
+                        <CoinIcon />
+                        <span>80</span>
+                    </>
+                ) : null}
+                <span>获得趋势分析</span>
+            </motion.button>
+        </div>
+    );
+}
 
 function FormContent() {
     return (
@@ -12,6 +59,7 @@ function FormContent() {
                 <LeagueDrawer />
                 <Form />
             </div>
+            <SubmitButton />
         </div>
     );
 }
