8cd8c0bb99f5c38b3daa45bb9bf87503aabd0e7a
susanna <susanna@future8.com>
2024-01-24 11:32:56 +0800

    feat: add big data analysis result layout

--- apps/site-03/app/aiBigData/analysisResult/analysisResult.tsx
+++ apps/site-03/app/aiBigData/analysisResult/analysisResult.tsx
@@ -1,13 +1,355 @@
+'use client';
 import Link from 'next/link';
 import Bodan from '../components/bodan/bodan';
+import { Tabs, Tab } from 'ui';
+import { useRouter } from 'next/navigation';
+import { useEffect, useMemo, useState } from 'react';
+import type { GetFootballStatsRequest } from 'data-center';
+import { getFootballStats, getMemberInfo } from 'data-center';
+import dayjs from 'dayjs';
+import { useUserStore } from '@/store/userStore';
+import { useQueryFormStore } from '../queryFormStore';
+import { useMatchFilterStore } from '../matchFilterStore';
+import ErrorDialog from '../components/dialog/dialog';
+import style from './analysisResult.module.scss';
+// import Handicap from './(dashboard)/handicap/handicap';
+import { useAnalyticsResultStore } from './analysisResultStore';
+// import ContestDrawerList from './components/contestDrawerList/contestDrawerList';
+// import Tutorial from './tutorial';
+import SystemErrorImage from './img/systemError.svg';
+import EmptyDataImage from './img/emptyData.svg';
+import AnalyzeDataImage from './img/analyzeData.svg';
+
+function InsufficientBalance() {
+    const router = useRouter();
+    const setOpenNormalDialog = useAnalyticsResultStore.use.setOpenNormalDialog();
+
+    return (
+        <>
+            <div className={style.dialogMessage}>
+                <p>余额不足，请充值</p>
+            </div>
+            <div className={style.footer}>
+                <div
+                    className={style.close}
+                    onClick={() => {
+                        setOpenNormalDialog(false);
+                        router.push('/aiBigData/queryForm');
+                    }}
+                >
+                    取消
+                </div>
+
+                <div className={style.confirm}>
+                    <Link href="/userInfo/subscribe">去充值</Link>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function EmptyResponseError() {
+    const router = useRouter();
+    const setOpenNormalDialog = useAnalyticsResultStore.use.setOpenNormalDialog();
+    const isVip = useUserStore.use.memberSubscribeStatus().planId;
+
+    return (
+        <>
+            <div className={style.dialogMessage}>
+                <EmptyDataImage />
+                {!isVip && <p className={style.refund}>已退款</p>}
+                <p>
+                    此條件查无资料
+                    <br />
+                    请重新修改搜寻条件
+                </p>
+            </div>
+            <div className={style.footer}>
+                <div
+                    className={style.close}
+                    onClick={() => {
+                        setOpenNormalDialog(false);
+                        router.push('/aiBigData/queryForm');
+                    }}
+                >
+                    重新查询
+                </div>
+            </div>
+        </>
+    );
+}
+
+function SystemError() {
+    const router = useRouter();
+    const [message, setMessage] = useState('');
+    const setOpenNormalDialog = useAnalyticsResultStore.use.setOpenNormalDialog();
+    const dialogErrorType = useAnalyticsResultStore.use.dialogContentType();
+
+    useEffect(() => {
+        switch (dialogErrorType) {
+            case 'system':
+                setMessage('哎呀，系统暂时出错！ 请稍候重试');
+                break;
+            case 'parameter':
+                setMessage('参数错误，请重新选择');
+                break;
+        }
+    }, [dialogErrorType]);
+
+    return (
+        <>
+            <div className={style.dialogMessage}>
+                <SystemErrorImage />
+                <p>{message}</p>
+            </div>
+            <div className={style.footer}>
+                <div
+                    className={style.close}
+                    onClick={() => {
+                        setOpenNormalDialog(false);
+                        router.push('/aiBigData/queryForm');
+                    }}
+                >
+                    返回
+                </div>
+                <div
+                    className={style.confirm}
+                    onClick={() => {
+                        setOpenNormalDialog(false);
+                        router.push('/aiBigData/queryForm');
+                    }}
+                >
+                    回报错误
+                </div>
+            </div>
+        </>
+    );
+}
 
 function AnalysisResult() {
+    const router = useRouter();
+    const defaultPageIndex = useAnalyticsResultStore.use.defaultPageIndex();
+    const setDefaultPageIndex = useAnalyticsResultStore.use.setDefaultPageIndex();
+    const showContestDrawer = useAnalyticsResultStore.use.showContestDrawer();
+    // const setShowContestDrawer = useAnalyticsResultStore.use.setShowContestDrawer();
+    // const selectedResult = useAnalyticsResultStore.use.selectedResult();
+    // const contestList = useAnalyticsResultStore.use.contestList();
+    const showedTutorial = useAnalyticsResultStore.use.showedTutorial();
+    const setShowedTutorial = useAnalyticsResultStore.use.setShowedTutorial();
+
+    const endDate = useQueryFormStore.use.endDate();
+    const startDate = useQueryFormStore.use.startDate();
+    const teamSelected = useQueryFormStore.use.teamSelected();
+    const teamHandicapOdds = useQueryFormStore.use.teamHandicapOdds();
+    const handicapOddsSelected = useQueryFormStore.use.handicapOddsSelected();
+    const loading = useQueryFormStore.use.loading();
+    const setLoading = useQueryFormStore.use.setLoading();
+    const setAnalysisResultData = useAnalyticsResultStore.use.setAnalysisResultData();
+    const setHandicapEchart = useAnalyticsResultStore.use.setHandicapEchart();
+    const isAnalysisBySearch = useQueryFormStore.use.isAnalysisBySearch();
+    const leagueIds = useMatchFilterStore.use.selectedleagueIdList();
+
+    const setDialogContent = useAnalyticsResultStore.use.setDialogContent();
+    const dialogErrorType = useAnalyticsResultStore.use.dialogContentType();
+    const dialogContent = useAnalyticsResultStore.use.dialogContent();
+    const setOpenNormalDialog = useAnalyticsResultStore.use.setOpenNormalDialog();
+    const openNoramlDialog = useAnalyticsResultStore.use.openNoramlDialog();
+    const analysisRecord = useAnalyticsResultStore.use.analysisResultData();
+    const checkboxState = useQueryFormStore.use.checkboxState();
+    const { handicap, overUnder } = checkboxState;
+    const setDialogContentType = useAnalyticsResultStore.use.setDialogContentType();
+    const setUserInfo = useUserStore.use.setUserInfo();
+    const allowSlideScroll = useAnalyticsResultStore.use.tabSlideScroll();
+
+    useEffect(() => {
+        setShowedTutorial(Boolean(localStorage.getItem('showAnalysisTutorial')));
+    }, []);
+
+    const tabStyle = {
+        gap: 4,
+        swiperOpen: true,
+        scrolling: true,
+        buttonRadius: 30,
+        allowSlideScroll
+    };
+
+    const tabList = useMemo(
+        () => [
+            {
+                label: '让分/大小趨勢',
+                // content: <Handicap />,
+                params: 'handicap'
+            },
+            {
+                label: '15分钟进球',
+                content: <>15分钟</>,
+                params: 'minutes'
+            },
+            {
+                label: '波胆',
+                content: <Bodan />,
+                params: 'bodan'
+            }
+        ],
+        []
+    );
+
+    const handlePlanTabClick = (tabName: string) => {
+        const index = tabList.findIndex(item => item.params === tabName);
+        setDefaultPageIndex(index || 0);
+    };
+
+    const getUserInfo = async () => {
+        const res = await getMemberInfo();
+        if (res.success) {
+            setUserInfo(res.data);
+        }
+    };
+
+    const fetchData = async () => {
+        // API完成後需要傳的參數
+        const query: GetFootballStatsRequest = {
+            mission: 'create',
+            leagues: leagueIds,
+            startTime: dayjs(startDate).unix(),
+            endTime: dayjs(endDate).unix()
+        };
+        if (handicap) {
+            query.handicapSide = teamSelected.length >= 2 ? 'all' : teamSelected[0];
+            query.handicapValues = teamHandicapOdds;
+        }
+        if (overUnder) {
+            query.overUnderValues = handicapOddsSelected;
+        }
+
+        setLoading(true);
+        const res = await getFootballStats(query);
+
+        if (!res.success) {
+            setTimeout(() => {
+                setOpenNormalDialog(true);
+                setLoading(false);
+                setAnalysisResultData(undefined);
+            }, 500);
+            return;
+        }
+
+        if (res.data.errorStatus) {
+            let dialogType = 'system';
+            switch (res.data.errorStatus) {
+                case '0':
+                    dialogType = 'system'; // 系統錯誤
+                    break;
+                case '1':
+                    dialogType = 'parameter'; // 參數錯誤
+                    break;
+                case '2':
+                    dialogType = 'empty'; //沒有資料
+                    break;
+                case '3':
+                    dialogType = 'balance'; // 餘額不足
+                    break;
+                default:
+                    break;
+            }
+            setDialogContentType(dialogType);
+            setOpenNormalDialog(true);
+            setLoading(false);
+            setAnalysisResultData(undefined);
+            return;
+        }
+
+        setAnalysisResultData(res.data);
+        setHandicapEchart(res.data);
+        setLoading(false);
+        void getUserInfo();
+    };
+
+    useEffect(() => {
+        // 代表不是從搜尋導到這頁，可能是重整頁面
+        if (!isAnalysisBySearch && !showContestDrawer) {
+            router.push('/aiBigData/queryForm');
+        }
+
+        if (!isAnalysisBySearch) return;
+        void fetchData();
+    }, []);
+
+    useEffect(() => {
+        switch (dialogErrorType) {
+            case 'system':
+            case 'parameter':
+                setDialogContent(<SystemError />);
+                break;
+            case 'empty':
+                setDialogContent(<EmptyResponseError />);
+                break;
+            case 'balance':
+                setDialogContent(<InsufficientBalance />);
+                break;
+            default:
+                setDialogContent(null);
+                break;
+        }
+    }, [dialogErrorType, setDialogContent]);
+
     return (
-        <div>
-            智能分析結果頁
-            <Link href="/football/4053571/analyze">去賽事</Link>
-            <Bodan />
-        </div>
+        <>
+            <div className={style.dashboard}>
+                <Tabs
+                    allowSlideScroll={tabStyle.allowSlideScroll}
+                    buttonRadius={tabStyle.buttonRadius}
+                    defaultValue={defaultPageIndex}
+                    gap={tabStyle.gap}
+                    onTabChange={value => {
+                        handlePlanTabClick(value);
+                    }}
+                    position="flexStart"
+                    scrolling={tabStyle.scrolling}
+                    styling="underline"
+                    swiperOpen={tabStyle.swiperOpen}
+                >
+                    {tabList.map(item => {
+                        return (
+                            <Tab key={item.params} label={item.label} value={item.params}>
+                                {loading ? (
+                                    <div className={style.analyze}>
+                                        <AnalyzeDataImage />
+                                        <span>资料分析中 请稍候</span>
+                                    </div>
+                                ) : (
+                                    item.content
+                                )}
+                            </Tab>
+                        );
+                    })}
+                </Tabs>
+            </div>
+            {!showedTutorial && analysisRecord ? (
+                <div className={style.tutorialBlock}>
+                    {/* <Tutorial setDefaultPageIndex={setDefaultPageIndex} /> */}
+                </div>
+            ) : null}
+            <ErrorDialog
+                content={<div className={style.dialogContent}>{dialogContent}</div>}
+                onClose={() => {
+                    setOpenNormalDialog(false);
+                }}
+                openDialog={openNoramlDialog}
+            />
+
+            {/* <ContestDrawerList
+                isOpen={showContestDrawer}
+                matchList={contestList}
+                onClose={() => {
+                    setShowContestDrawer(false);
+                }}
+                onOpen={() => {
+                    setShowContestDrawer(true);
+                }}
+                selectedResult={selectedResult}
+            /> */}
+        </>
     );
 }
 
--- apps/site-03/app/aiBigData/analysisResult/layout.tsx
+++ apps/site-03/app/aiBigData/analysisResult/layout.tsx
@@ -1,8 +1,113 @@
 'use client';
-import type { ReactNode } from 'react';
+import { useEffect, type ReactNode } from 'react';
+import Chip from '@mui/material/Chip';
+import { useMatchFilterStore } from '../matchFilterStore';
+import { createAnalysisResultStore } from './analysisResultStore';
+import { creatMatchFilterStore } from './matchFilterStore';
+import style from './layout.module.scss';
+import CloseIcon from './img/close.svg';
+import { useQueryFormStore } from '@/app/aiBigData/queryFormStore';
+import Header from '@/components/header/headerTransparent';
+
+type HandicapSideType = 'home' | 'away';
+
+function CreateStore({ children }: { children: ReactNode }) {
+    createAnalysisResultStore({
+        analysisResultData: undefined
+    });
+
+    creatMatchFilterStore({
+        contestList: [],
+        contestInfo: {}
+    });
+
+    return <>{children}</>;
+}
+
+function QueryInfo({ children }: { children: ReactNode }) {
+    const endDate = useQueryFormStore.use.endDate();
+    const startDate = useQueryFormStore.use.startDate();
+    const teamSelected = useQueryFormStore.use.teamSelected();
+    const teamHandicapOdds = useQueryFormStore.use.teamHandicapOdds();
+    const handicapOddsSelected = useQueryFormStore.use.handicapOddsSelected();
+    const checkboxState = useQueryFormStore.use.checkboxState();
+    const { handicap, overUnder } = checkboxState;
+    const selectedleagueIdList = useMatchFilterStore.use.selectedleagueIdList();
+    const contestInfo = useMatchFilterStore.use.contestInfo();
+    const setFilterSelected = useMatchFilterStore.use.setFilterSelected();
+
+    const handicapTeam = {
+        home: '主',
+        away: '客'
+    };
+
+    const handleDelete = (leagueName: string) => {
+        setFilterSelected(leagueName, 'league');
+    };
+
+    return (
+        <CreateStore>
+            <div className={style.bigDataGame}>
+                <div className={style.column}>
+                    <div className={style.row}>
+                        <span className={style.title}>联赛</span>
+                        <span className={style.name}>
+                            {selectedleagueIdList.map(id => {
+                                return (
+                                    <Chip
+                                        className={style.tag}
+                                        deleteIcon={<CloseIcon />}
+                                        key={id}
+                                        label={contestInfo[id].leagueChsShort}
+                                        onDelete={() => {
+                                            handleDelete(contestInfo[id].leagueChsShort);
+                                        }}
+                                    />
+                                );
+                            })}
+                        </span>
+                    </div>
+                    <div className={style.row}>
+                        <span className={style.title}>全场让分</span>
+                        <span className={style.name}>
+                            让方/
+                            {teamSelected.length >= 2 || !handicap
+                                ? '不挑选'
+                                : handicapTeam[teamSelected[0] as HandicapSideType]}
+                            、盘口/
+                            {handicap ? teamHandicapOdds || teamHandicapOdds === '0' : '不挑选'}
+                        </span>
+                    </div>
+                    <div className={style.row}>
+                        <span className={style.title}>全场大小</span>
+                        <span className={style.name}>
+                            {overUnder ? handicapOddsSelected : '不挑選'}
+                        </span>
+                    </div>
+                    <div className={style.row}>
+                        <span className={style.title}>时间区间</span>
+                        <span className={style.date}>
+                            {startDate} ~ {endDate}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            {children}
+        </CreateStore>
+    );
+}
 
 function AnalysisResultLayout({ children }: { children: ReactNode }) {
-    return <>{children}</>;
+    useEffect(() => {
+        window.scroll(0, 0);
+    }, []);
+
+    return (
+        <div className={style.resultLayout}>
+            <Header title="分析结果" />
+            <QueryInfo>{children}</QueryInfo>
+        </div>
+    );
 }
 
 export default AnalysisResultLayout;
