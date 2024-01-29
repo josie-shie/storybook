091419e462640842c6f1e73296f61b8bb9d4c8c4
Zafer <zafer@befuture8.com>
2024-01-29 10:52:05 +0800

    feat: change payDrawer image

--- apps/site-03/components/confirmPayDrawer/confirmPayDrawer.tsx
+++ apps/site-03/components/confirmPayDrawer/confirmPayDrawer.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image';
 import { Button } from '@mui/material';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import { useUserStore } from '@/store/userStore';
 import style from './confirmPayDrawer.module.scss';
-import Star from './img/star.png';
-import PayHand from './img/payHand.png';
+import Soccer from './img/soccer.svg';
+import Coin from './img/coin.svg';
 
 interface ConfirmPayDrawerProps {
     title?: string;
@@ -43,13 +42,13 @@ function ConfirmPayDrawer({
         >
             <div className={style.confirmPay}>
                 <div className={style.PayHandImg}>
-                    <Image alt="payHand" height={100} src={PayHand} width={100} />
+                    <Soccer />
                 </div>
                 <div className={style.payContent}>
                     <div className={style.price}>
                         <span className={style.text}>支付</span>
                         <span className={style.number}>
-                            <Image alt="priceLogo" className={style.image} src={Star} width={14} />
+                            <Coin className={style.star} />
                             {price}
                         </span>
                     </div>
@@ -59,13 +58,7 @@ function ConfirmPayDrawer({
                     我的余额: {userInfo.balance ? userInfo.balance : 0}金币
                 </div>
                 <Button className={style.payBtn} fullWidth onClick={onPay}>
-                    <Image
-                        alt="video_icon"
-                        className={style.icon}
-                        height={17}
-                        src={Star}
-                        width={17}
-                    />
+                    <Coin className={style.star} />
                     <span>支付</span>
                 </Button>
             </div>
