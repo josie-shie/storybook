f9a551c8a97afa1144385547a2d1d9902394478e
Ben <ben@befuture8.com>
2024-02-06 19:29:16 +0800

    fix: notice bug fix userInfo defaultAvatar

--- apps/site-03/app/notice/chat/chat.tsx
+++ apps/site-03/app/notice/chat/chat.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import type { MessageResponse } from 'lib';
 import { messageService, getMessageResponse, cancelMessage } from 'lib';
 import NoData from '@/components/baseNoData/noData';
@@ -7,12 +7,14 @@ import { useNoticeStore } from '../noticeStore';
 import style from '../notice.module.scss';
 import ChatCard from './components/chatCard';
 import ChatInfo from './components/chatInfo';
+import SkeletonLayout from './components/skeleton/skeleton';
 
 function ChatList() {
     const firstTimeRef = useRef(true);
     const editStatus = useNoticeStore.use.editStatus();
     const chatList = useNoticeStore.use.chatList();
     const setChatList = useNoticeStore.use.setChatList();
+    const [isNoData, setIsNoData] = useState<boolean | null>(null);
 
     useEffect(() => {
         if (firstTimeRef.current) {
@@ -42,6 +44,7 @@ function ChatList() {
             if (res.action === 'get_room_list') {
                 if (res.rooms) {
                     setChatList(res.rooms);
+                    setIsNoData(res.rooms.length === 0);
                 }
             }
         };
@@ -51,16 +54,22 @@ function ChatList() {
         };
     }, [setChatList]);
 
-    if (chatList.length === 0) {
-        return <NoData text="暂无资料" />;
-    }
-
     return (
-        <ul className={`${style.noticeList} ${editStatus && style.isEdit}`}>
-            {chatList.map(chat =>
-                chat.lastMessages.length > 0 ? <ChatCard chatData={chat} key={chat.roomId} /> : null
+        <>
+            {chatList.length === 0 && isNoData === null && <SkeletonLayout />}
+
+            {chatList.length === 0 && isNoData ? (
+                <NoData text="暂无资料" />
+            ) : (
+                <ul className={`${style.noticeList} ${editStatus && style.isEdit}`}>
+                    {chatList.map(chat =>
+                        chat.lastMessages.length > 0 ? (
+                            <ChatCard chatData={chat} key={chat.roomId} />
+                        ) : null
+                    )}
+                </ul>
             )}
-        </ul>
+        </>
     );
 }
 
---
+++ apps/site-03/app/notice/chat/components/skeleton/skeleton.tsx
@@ -0,0 +1,48 @@
+import Skeleton from '@mui/material/Skeleton';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import style from './skeleton.module.scss';
+
+function SkeletonLayout() {
+    const data = [1, 2, 3];
+
+    return (
+        <div className={style.container}>
+            {data.map(item => {
+                return (
+                    <Stack className={style.skeletonLayout} key={item} spacing={1}>
+                        <div className={style.top}>
+                            <div>
+                                <Skeleton
+                                    animation="wave"
+                                    height={40}
+                                    variant="circular"
+                                    width={40}
+                                >
+                                    <Avatar />
+                                </Skeleton>
+                            </div>
+                            <div className={style.title}>
+                                <Skeleton
+                                    animation="wave"
+                                    height={18}
+                                    sx={{ marginBottom: '8px' }}
+                                    variant="rounded"
+                                    width="100%"
+                                />
+                                <Skeleton
+                                    animation="wave"
+                                    height={18}
+                                    variant="rounded"
+                                    width="100%"
+                                />
+                            </div>
+                        </div>
+                    </Stack>
+                );
+            })}
+        </div>
+    );
+}
+
+export default SkeletonLayout;
