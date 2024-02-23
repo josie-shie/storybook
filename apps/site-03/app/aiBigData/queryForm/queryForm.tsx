903a2cf531a8901150631d308bdd1f8b0ef90f62
josie <josie@newfuturetv.com>
2024-02-23 10:58:32 +0800

    fix(MatchFilterDrawer): remove space

--- apps/site-03/app/aiBigData/queryForm/queryForm.tsx
+++ apps/site-03/app/aiBigData/queryForm/queryForm.tsx
@@ -160,7 +160,6 @@ function SubmitButton({ setZeroMatchList }: { setZeroMatchList: (list: number[])
 
     const handleCheckMatchesCount = async () => {
         if (!selectedleagueIdList.length) return;
-
         let handicapSideValue = '';
         if (teamSelected.length === 2) {
             handicapSideValue = 'all';
