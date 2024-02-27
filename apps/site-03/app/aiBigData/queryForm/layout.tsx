81b3081446540a77e644cdee065a95d340e82a41
Ben <ben@befuture8.com>
2024-02-27 17:43:07 +0800

    feat: add rule detail

--- apps/site-03/app/aiBigData/queryForm/layout.tsx
+++ apps/site-03/app/aiBigData/queryForm/layout.tsx
@@ -1,14 +1,16 @@
 'use client';
 import type { ReactNode } from 'react';
 import Header from '@/components/header/headerTransparent';
+import Footer from '@/components/footer/footer';
+import Rule from '../components/rule/rule';
 import style from './layout.module.scss';
 import AnimationBanner from './components/animationBanner/animationBanner';
-import Footer from '@/components/footer/footer';
 
 function QueryFormLayout({ children }: { children: ReactNode }) {
     return (
         <div className={style.queryFormLayout}>
             <Header back={false} title="智能分析" />
+            <Rule />
             <AnimationBanner />
             {children}
             <Footer />
--- apps/site-03/app/guess/detail/[matchId]/components/rule/rule.tsx
+++ apps/site-03/app/guess/detail/[matchId]/components/rule/rule.tsx
@@ -21,18 +21,50 @@ function Rule() {
                     <span onClick={handleClickOpen}>规则</span>
                 </div>
             </ButtonBase>
-            <Dialog onClose={handleClose} open={open}>
-                <DialogTitle>规则说明</DialogTitle>
+            <Dialog
+                PaperProps={{
+                    style: {
+                        borderRadius: '15px',
+                        width: '90%'
+                    }
+                }}
+                onClose={handleClose}
+                open={open}
+            >
                 <div className={style.ruleContent}>
-                    <p>
-                        參與者預測比賽結果，通常包括主客比分、勝負或其他特定事件。
-                        <br />
-                        比賽結束後，如果預測正確，參與者贏得獎金，否則失去投注金額。
-                        <br />
-                        公平性、透明度和合法性為本站核心原則。
-                        <br />
-                        請玩家謹慎參與，不要超出財務承受範圍。
-                    </p>
+                    <div className={style.title}>规则</div>
+                    <div className={style.row}>
+                        <h1>每日竞猜机会</h1>
+                        <ul>
+                            <li>每位用户每天拥有五次参与猜球的机会。</li>
+                            <li>猜球的次数会在每天的中午12:00重置。</li>
+                        </ul>
+                    </div>
+                    <div className={style.row}>
+                        <h1>竞猜玩法</h1>
+                        <ul>
+                            <li>猜胜负：预测比赛的胜方或是否平局。</li>
+                            <li>猜总进球：预测比赛的总进球数量。</li>
+                            <li>每场比赛的个别玩法限竞猜一次。</li>
+                        </ul>
+                    </div>
+                    <div className={style.row}>
+                        <h1>解锁高胜率玩家风向比例</h1>
+                        <ul>
+                            <li>完成至少一次竞猜后，将可查看高胜率玩家的竞猜选择比例及人数。</li>
+                            <li>可以提供您参考，以便作出更加明智的竞猜决策。</li>
+                        </ul>
+                    </div>
+                    <div className={style.row}>
+                        <h1>其他注意事项</h1>
+                        <ul>
+                            <li>请于比赛开始前完成竞猜。</li>
+                            <li>当日未使用的竞猜机会将不会累积至隔日。</li>
+                        </ul>
+                    </div>
+                    <button className={style.close} onClick={handleClose} type="button">
+                        关闭
+                    </button>
                 </div>
             </Dialog>
         </>
