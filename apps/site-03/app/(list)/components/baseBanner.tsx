554365621d95cd2e6c73b33a088b6343a3390a38
Peter <peter>
2024-03-01 17:40:37 +0800

    perf(banner): load

--- apps/site-03/app/(list)/components/baseBanner.tsx
+++ apps/site-03/app/(list)/components/baseBanner.tsx
@@ -1,8 +1,7 @@
 import Image from 'next/image';
 import { getRandomInt } from 'lib';
-import { useEffect, useState, memo } from 'react';
+import { memo } from 'react';
 import Link from 'next/link';
-import Skeleton from '@mui/material/Skeleton';
 import { useUserStore } from '@/store/userStore';
 import style from './baseBanner.module.scss';
 import GuessBannerImage from './img/guessBanner.jpg';
@@ -44,32 +43,23 @@ function getRandomImageConfig(isLogin: boolean) {
 }
 
 function BaseBanner({ className }: { className: string }) {
-    const [isMounted, setIsMounted] = useState(false);
     const isLogin = useUserStore.use.isLogin();
 
     const randomImage = getRandomImageConfig(isLogin);
     const maxRandomNumber = isLogin ? 1 : 2;
     const randomNumberInit = getRandomInt(1, maxRandomNumber) as 1 | 2;
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
     const randomMap = randomImage[randomNumberInit];
 
     return (
         <div className={`${style.baseBanner} ${className}`}>
-            {isMounted ? (
-                <Link className={style.banner} href={randomMap?.link || defaultConfig.link}>
-                    <Image
-                        alt={randomMap?.label || defaultConfig.label}
-                        priority
-                        src={randomMap?.image || defaultConfig.image}
-                    />
-                </Link>
-            ) : (
-                <Skeleton animation="wave" height="100%" variant="rectangular" width="100%" />
-            )}
+            <Link className={style.banner} href={randomMap?.link || defaultConfig.link}>
+                <Image
+                    alt={randomMap?.label || defaultConfig.label}
+                    priority
+                    src={randomMap?.image || defaultConfig.image}
+                />
+            </Link>
         </div>
     );
 }
